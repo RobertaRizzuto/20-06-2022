fbe20e2aee04b9e9f09c7e5024e922c984ecfca6
RobertaRizzuto <rtarizzuto@gmail.com>
2022-06-20 15:37:24 +0200

    fix:undefined fixes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function sum2(numOne, numTwo) {
     console.log(numOne + numTwo);
   }
 }
-console.log(sum2(1, 3));
+sum2(1, 3);
 
 
 // 3 -> Scriviamo come per il punto due, anche per moltiplicazione, divisione, e sottrazione
@@ -27,7 +27,7 @@ function sub(numOne, numTwo) {
     console.log(numOne - numTwo);
   }
 }
-console.log(sub(1, 3));
+sub(1, 3);
 
 function mul(numOne, numTwo) {
   if (isNaN(numOne) || isNaN(numTwo)) {
@@ -36,7 +36,7 @@ function mul(numOne, numTwo) {
     console.log(numOne * numTwo);
   }
 }
-console.log(mul(1, 3));
+mul(1, 3);
 
 function div(numOne, numTwo) {
   if (isNaN(numOne) || isNaN(numTwo)) {
@@ -47,15 +47,15 @@ function div(numOne, numTwo) {
     console.log(numOne / numTwo);
   }
 }
-console.log(div(1, 3));
-console.log(div(1, 0));
+div(1, 3);
+div(1, 0);
 // 4 -> Scriviamo una funzione che possa eseguire le 4 precedenti
 
 const calculator = function (operation, numOne, numTwo) {
   operation(numOne, numTwo)
 };
 
-console.log(calculator(sum2, 3, 3)); // per stampare l'addizione
-console.log(calculator(sub, 1, 3)); // per stampare la sottrazione
-console.log(calculator(mul, 1, 3));   // per stampare la moltiplicazione
-console.log(calculator(div, 1, 3)); // per stampare la divisione
+calculator(sum2, 3, 3); // per stampare l'addizione
+calculator(sub, 1, 3); // per stampare la sottrazione
+calculator(mul, 1, 3);   // per stampare la moltiplicazione
+calculator(div, 1, 3); // per stampare la divisione
